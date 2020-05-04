3865fb1edc108eb52c5baec56ad8a91c27ff8455
Cassio.Placido <silenozlt@hotmail.com>
2020-05-04 13:29:27 -0300

    incluindo filtro por cidade, #8

--- analise.py
+++ analise.py
@@ -36,7 +36,7 @@ def main():
         #cidade = st.text_input("Qual cidade deseja analisar ? ")
         #if cidade is not False:
         city = '';
-        city = st.selectbox('Selecione a cidade para visualizar', list(covid.cidade), default=[1])
+        city = st.selectbox('Selecione a cidade para visualizar', list(covid.cidade))
         covid_cidade = covid.query("cidade== city")
         covid_casos_cidade = covid_cidade[["data", "casos", "mortes"]]
         covid_casos_cidade.head()
